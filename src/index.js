c6ae83195c5bbb8d4b04117c61ae60b90ccb2e92
AlexOlga <bulbenko@yandex.ru>
2022-01-31 10:57:08 +0300

    feat: add morse-decoder

--- src/index.js
+++ src/index.js
@@ -39,6 +39,34 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const dash='-';
+    const dot='.';
+    let arr=[];
+    let newArr=[];
+    let n=0;
+    //преобразуем строку в массив
+ for (let i=0; i < expr.length; i=i+10 )   {
+     arr[n]= expr.slice(i, i+10);
+     n++;     
+ }
+ // делаем массив вида -.
+ newArr=arr.map((item)=>{
+let s='';
+for (let i=0; i < item.length; i=i+2 )   {
+    if (item.slice(i, i+2)=='**') {return ' '}
+    else if (item.slice(i, i+2)=='11') {s=s+ '-'}
+    else if (item.slice(i, i+2)=='10') {s=s+ '.'}       
+}
+return s;
+});
+let result='';
+newArr.forEach((item)=>{
+if (item==' ') {result=result+' '; }
+else {result=result+MORSE_TABLE[item] }
+console.log('item',item);
+console.log(MORSE_TABLE[item]);}
+);
+ return result;   
 }
 
 module.exports = {
